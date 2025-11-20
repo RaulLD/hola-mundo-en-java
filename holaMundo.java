6b874e4fc1e3a51e29c2316bdce2b78dafc66eda
Raúl Lumbreras Delegido <rlumdel0802@g.educaand.es>
2025-11-20 18:49:09 +0100

    Comentarios añadidos al código

--- holaMundo.java
+++ holaMundo.java
@@ -1,3 +1,9 @@
+/**
+ * Muestra por pantalla la frase "¡Hola Mundo!"
+ *
+ * @author Raúl Lumbreras
+ */
+
 public class holaMundo{
 	public static void main(String[] args){
 		System.out.print("¡Hola Mundo!");
